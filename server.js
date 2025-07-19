2eb6d569429c1dd2aaa38ec6c51ef330d8e8fae9
developeranass <anassartaj177@gmail.com>
2025-07-19 17:38:37 +0500

    Commiting messages

--- server.js
+++ server.js
@@ -6,7 +6,19 @@ app.get('/api/get', (req, res)=>{
     res.send({message: "hello back tpo nodejs"}) 
 });
 
- 
+
+app.get('/api/get_user_details', (req, res)=>{
+    res.send({
+        user: {
+            "name" : "Ayaz",
+            "age" : 22,
+            "contact": 12345
+        }
+
+
+    }) 
+});
+
 
 app.listen(8000, ()=> {
     console.log("Listening to 8000");
